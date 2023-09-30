cc15af9a191f7a12310a040674dc6ba6b6fe5e05
Jackson Serafim <jacksonvieiira12@gmail.com>
2023-09-30 19:28:00 -0300

    add puppy model

--- puppies/models.py
+++ puppies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Puppy(BaseModel):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
