a5b48a845908c6c1e26faa205ef098505627ad47
Jackson Serafim <jacksonvieiira12@gmail.com>
2023-10-01 08:58:19 -0300

    puppy detail base view

---
+++ puppies/tests/test_views.py
@@ -0,0 +1,8 @@
+from rest_framework.test import APITestCase
+from puppies.models import Puppy
+from puppies.serializers import PuppySerializer
+
+
+class ViewsTest(APITestCase):
+    def setUp(self):
+        pass
--- puppies/views.py
+++ puppies/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from puppies.models import Puppy
+from puppies.serializers import PuppySerializer
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+
+
+@api_view(['GET', 'DELTE', 'PUT'])
+def get_delete_update_puppy(request, pk):
+    puppy = get_object_or_404(Puppy, pk=pk)
+    if request.method == 'GET':
+        return Response({})
+    elif request.method == 'DELETE':
+        return Response({})
+    elif request.method == 'GET':
+        return Response({})
